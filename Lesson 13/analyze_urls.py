f817aed568fb1c405d91cebc290a6fdbd10bc767
Dima Leonov <leonovdima1998@gmail.com>
2023-06-11 18:13:04 +0300

    delete commented code

--- Lesson 13/analyze_urls.py
+++ Lesson 13/analyze_urls.py
@@ -22,33 +22,3 @@ class UrlAnalyzer(ParseData):
             elif response is not None and response.status_code == 200:
                 urls.get('valid').append(f'{i}\n')
         return urls
-
-
-    '''Здесь причина та же, что и в файле save_urls.py. Хотел сократить количество повторяющегося кода и ускорить 
-    работу программы. После того, как ты проверишь ДЗ - сделаю коммит и удалю старые куски кода.'''
-
-    # def _get_urls(self, data):
-    #     broken_urls = str()
-    #     if data[0] == '-url':
-    #         list_of_urls = self._get_list_of_urls_from_url(data[1])
-    #     elif data[0] == '-pdf':
-    #         list_of_urls = self._get_list_of_urls_from_pdf(data[1])
-    #     print(TEXT.PARSING_BROKEN_LINKS)
-    #     for i in list_of_urls:
-    #         response = self._go_to_url(i)
-    #         if response is None or response.status_code != 200:
-    #             broken_urls += f'{i}\n'
-    #     return broken_urls
-
-    # def _get_urls(self, data):
-    #     valid_urls = str()
-    #     if data[0] == '-url':
-    #         list_of_urls = self._get_list_of_urls_from_url(data[1])
-    #     elif data[0] == '-pdf':
-    #         list_of_urls = self._get_list_of_urls_from_pdf(data[1])
-    #     print(TEXT.PARSING_VALID_LINKS)
-    #     for i in list_of_urls:
-    #         response = self._go_to_url(i)
-    #         if response is not None and response.status_code == 200:
-    #             valid_urls += f'{i}\n'
-    #     return valid_urls
